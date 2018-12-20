5e8dd589301179284dac4bfeb2b4431130d17bbb
zvezdochiot <mykaralw@yandex.ru>
2018-12-20 23:47:50 +0300

    0.2a "clean"
    Clean repo.

--- Release/src/SymmetricSurroundSaliency.d
+++
@@ -1,6 +0,0 @@
-src/SymmetricSurroundSaliency.d: ../src/SymmetricSurroundSaliency.cpp \
-  ../src/SaliencyDetector.h ../src/SymmetricSurroundSaliency.h
-
-../src/SaliencyDetector.h:
-
-../src/SymmetricSurroundSaliency.h:
